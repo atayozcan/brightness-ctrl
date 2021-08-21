548e000371230a295b4abe3f5eda475db59ceee8
Atay Özcan <atayozcan@pm.me>
2021-08-21 16:34:52 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::error::Error;
+use zbus::{dbus_proxy, Connection};
+use zvariant::Value;
+
+#[dbus_proxy]
+trait Notifications {
+    fn notify(
+        &self,
+        app_name: &str,
+        replaces_id: u32,
+        app_icon: &str,
+        summary: &str,
+        body: &str,
+        actions: &[&str],
+        hints: HashMap<&str, &Value>,
+        expire_timeout: i32,
+    ) -> zbus::Result<u32>;
+
+    fn close_notification(&self, id: u32) -> zbus::Result<u32>;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for _i in 0..1000 {
+        NotificationsProxy::new(&Connection::new_session()?)?.notify(
+            "",
+            1,
+            "",
+            &*format!(
+                "Brightness: %{}",
+                std::fs::read_to_string("/sys/class/backlight/nvidia_0/actual_brightness")?
+            ),
+            "",
+            &[],
+            HashMap::new(),
+            0,
+        )?;
+    }
+
+    NotificationsProxy::new(&Connection::new_session()?)?.close_notification(1)?;
+    Ok(())
+}
