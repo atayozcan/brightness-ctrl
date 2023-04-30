3d2822c0d8ed7b59d9c7aa90873c240784a98b79
atay <atayozcan@pm.me>
2023-04-30 20:31:37 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
-use std::{collections::HashMap, error::Error};
-use zbus::{dbus_proxy, Connection};
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use zbus::Connection;
 use zvariant::Value;
 
-#[dbus_proxy]
-trait Notifications {
-    fn notify(
-        &self,
-        app_name: &str,
-        replaces_id: u32,
-        app_icon: &str,
-        summary: &str,
-        body: &str,
-        actions: &[&str],
-        hints: HashMap<&str, &Value>,
-        expire_timeout: i32,
-    ) -> zbus::Result<u32>;
-
-    fn close_notification(&self, id: u32) -> zbus::Result<u32>;
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    for _i in 0..1000 {
-        NotificationsProxy::new(&Connection::new_session()?)?.notify(
-            "",
-            1,
-            "",
-            &*format!(
-                "Brightness: %{}",
-                std::fs::read_to_string("/sys/class/backlight/nvidia_0/actual_brightness")?
+fn main() {
+    let percent = format!(
+        "Brightness: %{}",
+        read_to_string("/sys/class/backlight/nvidia_0/actual_brightness").unwrap()
+    );
+    Connection::new_session()
+        .unwrap()
+        .call_method(
+            Some("org.freedesktop.Notifications"),
+            "/org/freedesktop/Notifications",
+            Some("org.freedesktop.Notifications"),
+            "Notify",
+            &(
+                "",
+                0u32,
+                "",
+                percent,
+                "",
+                vec![""; 0],
+                HashMap::<&str, &Value>::new(),
+                5000,
             ),
-            "",
-            &[],
-            HashMap::new(),
-            0,
-        )?;
-    }
-
-    NotificationsProxy::new(&Connection::new_session()?)?.close_notification(1)?;
-    Ok(())
+        )
+        .unwrap();
 }
